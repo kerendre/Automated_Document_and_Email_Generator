e0844d818e888759b80557a3926025186395b918
Drevin Keren <drevinkeren@gmail.com>
2023-02-12 23:00:18 +0200

    fully working project creating docx file with 2 new pics and table

--- adding_objects_file.py
+++ adding_objects_file.py
@@ -1,78 +1,247 @@
+
 import docx
 import os
 import pandas as pd
 
+def Add_pic(doc,pic_name,pic_description):
+    ''' Add pic to document'''
 
-def Add_pic(doc, pic_name, pic_description):
-    '''
-    Add a picture to the given docx document.
 
-    Parameters:
-        doc (docx.document.Document): The docx document object.
-        pic_name (str): The name of the picture file.
-        pic_description (str): The description to be added under the picture.
 
-    Returns:
-        None
-    '''
-    # Get the page width of the document
+    # Get the page width
     page_width = doc.sections[0].page_width
 
     # Add the picture to the Word document
     pic1 = doc.add_picture(f"PICS/{pic_name}")
 
-    # Fit the width of the first picture to 65% of the page width
+    # Fit the width of the first picture to the page width
     pic1.width = int(page_width * 0.65)
 
-    # Add a description under the picture
-    description_pic = doc.add_paragraph(pic_description)
+    # Add a description under the picture pic1
+    description_pic= doc.add_paragraph(pic_description)
 
     # Center the description
     description_pic.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
     # Add two lines of space
-    doc.add_paragraph("\n")
+    doc.add_paragraph("\n\n")
 
 
 def Add_table(doc, df, subtitle_to_the_table):
-    """
-    Add a table to the given docx document.
+    # Replace NaN values with an empty string
+    df = df.fillna('')
 
-    Parameters:
-        doc (docx.document.Document): The docx document object.
-        df (pandas.DataFrame): The pandas dataframe to be added as a table in the document.
-        subtitle_to_the_table (str): The title or subtitle for the table.
+    # Check if the dataframe is empty
+    if df.empty:
+        return
 
-    Returns:
-        None
-    """
-
-    # Add the title/subtitle to the table
     paragraph = doc.add_paragraph(subtitle_to_the_table)
     paragraph.style = doc.styles['Heading 1']
     paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
-    # Replace NaN values in the dataframe with an empty string
-    df = df.fillna('')
+    # Create the table
+    table = doc.add_table(rows=1, cols=len(df.columns))
 
-    # Add the dataframe to the document
-    doc.add_table(df.shape[0] + 1, df.shape[1])
-
-    # Add the header row
-    for j in range(df.shape[-1]):
-        header_cell = df.columns[j] if not pd.isna(df.columns[j]) else " "
-        # Replace header cell if it contains "Unnamed:" with an empty string
-        if "Unnamed:" in header_cell:
-            header_cell = " "
-
-        # Add the header cell to the table
-        doc.tables[0].cell(0, j).text = header_cell
-        # Make the header cell bold
-        doc.tables[0].cell(0, j).paragraphs[0].runs[0].bold = True
+    # Define the header row
+    header_row = table.rows[0]
+    header_row.style = 'Table Header'
+    for i, column_title in enumerate(df.columns):
+        header_row.cells[i].text = column_title
 
     # Add the data to the table
-    for i in range(df.shape[0]):
-        for j in range(df.shape[-1]):
-            # Add the cell data to the table
-            doc.tables[0].cell(i + 1, j).text = str(df.values[i, j])
+    for i, row in df.iterrows():
+        if row.isnull().all():
+            continue
 
+        new_row = table.add_row()
+        for j, cell_value in enumerate(row):
+            new_row.cells[j].text = str(cell_value)
+
+
+
+
+from docx import Document
+
+#
+# def Add_table(doc, df, subtitle_to_the_table):
+#     # Replace NaN values with an empty string
+#     df = df.fillna('')
+#
+#     paragraph = doc.add_paragraph(subtitle_to_the_table)
+#     paragraph.style = doc.styles['Heading 1']
+#     paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+
+
+
+    #
+    # # Add the header row
+    # #hdr_cells = table.rows[0].cells
+    # for header_index, header in enumerate(df.columns):
+    #     hdr_cells[header_index].text = header
+
+    # Add the data rows
+    #for row_index, row in df.iterrows():
+        # data_cells = table.rows[row_index + 1].cells
+        # if row.empty:
+        # #     continue
+        # for data_index, data in enumerate(row):
+        #     data_cells[data_index].text = str(data)
+
+
+        #doc.add_table(table)
+
+    #
+    # table = doc.add_table(rows=df.shape[0]+1, cols=df.shape[1])
+    # hdr_cells = table.rows[0].cells
+    # for i, column in enumerate(df.columns):
+    #     hdr_cells[i].text = column
+    # for i, row in df.iterrows():
+    #     cells = table.add_row().cells
+    #
+    #
+    #     for j, column in enumerate(df.columns):
+    #         cells[j].text = str(row[column])
+    #         if all(pd.isna(row)):
+    #             continue  # skip this iteration if the row is empty
+    #
+    # doc.add_paragraph(subtitle_to_the_table)
+    # #doc.add_table(table)
+    #
+
+
+
+
+# def Add_table(doc,df, subtitle_to_the_table):
+#     print("df,shape", (df.shape))
+#     # Add the title to the table
+#
+#     paragraph = doc.add_paragraph(subtitle_to_the_table)
+#     paragraph.style = doc.styles['Heading 1']
+#     paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+#
+#
+#
+#     # Replace NaN values with an empty string
+#     df = df.fillna('')
+#
+#
+#     # Add the dataframe to the document
+#     doc.add_table(df.shape[0] + 1, df.shape[1])
+#
+#     # Add the header row
+#     for j in range(df.shape[-1]):
+#         header_cell = df.columns[j] if not pd.isna(df.columns[j]) else " "
+#         if "Unnamed:" in header_cell:
+#             header_cell = " "
+#
+#         doc.tables[0].cell(0, j).text = header_cell
+#         doc.tables[0].cell(0, j).paragraphs[0].runs[0].bold = True
+#
+#
+#     ##
+#     print("(df.shape[0])",df.shape[0])
+#     print("df.shape[-1]",df.shape[1])
+#
+#     # Add the data
+#     for i in range(df.shape[0]):
+#         for j in range(df.shape[-1]):
+#
+#             print("i,j, str(df.values[i, j])", i,j, str(df.values[i, j]))
+#             doc.tables[0].cell(i + 1, j).text = str(df.values[i, j])
+#
+#
+
+
+
+
+
+
+
+# # #!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# #
+# # import docx
+# # import os
+# # import pandas as pd
+# #
+# #
+# # def Add_pic(doc, pic_name, pic_description):
+# #     '''
+# #     Add a picture to the given docx document.
+# #
+# #     Parameters:
+# #         doc (docx.document.Document): The docx document object.
+# #         pic_name (str): The name of the picture file.
+# #         pic_description (str): The description to be added under the picture.
+# #
+# #     Returns:
+# #         None
+# #     '''
+# #     # Get the page width of the document
+# #     page_width = doc.sections[0].page_width
+# #
+# #     # Add the picture to the Word document
+# #     pic1 = doc.add_picture(f"PICS/{pic_name}")
+# #
+# #     # Fit the width of the first picture to 65% of the page width
+# #     pic1.width = int(page_width * 0.65)
+# #
+# #     # Add a description under the picture
+# #     description_pic = doc.add_paragraph(pic_description)
+# #
+# #     # Center the description
+# #     description_pic.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+# #
+# #     # Add two lines of space
+# #     doc.add_paragraph("\n")
+# #
+# #
+# # def Add_table(doc, df, subtitle_to_the_table):
+# #     """
+# #     Add a table to the given docx document.
+# #
+# #     Parameters:
+# #         doc (docx.document.Document): The docx document object.
+# #         df (pandas.DataFrame): The pandas dataframe to be added as a table in the document.
+# #         subtitle_to_the_table (str): The title or subtitle for the table.
+# #
+# #     Returns:
+# #         None
+# #     """
+# #
+# #     # Add the title/subtitle to the table
+# #     paragraph = doc.add_paragraph(subtitle_to_the_table)
+# #     paragraph.style = doc.styles['Heading 1']
+# #     paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+# #
+# #     # Replace NaN values in the dataframe with an empty string
+# #     df = df.fillna('')
+# #
+# #     # Add the dataframe to the document
+# #     doc.add_table(df.shape[0] + 1, df.shape[1])
+# #
+# #     # Add the header row
+# #     for j in range(df.shape[-1]):
+# #         header_cell = df.columns[j] if not pd.isna(df.columns[j]) else " "
+# #         # Replace header cell if it contains "Unnamed:" with an empty string
+# #         if "Unnamed:" in header_cell:
+# #             header_cell = " "
+# #
+# #         # Add the header cell to the table
+# #         doc.tables[0].cell(0, j).text = header_cell
+# #         # Make the header cell bold
+# #         doc.tables[0].cell(0, j).paragraphs[0].runs[0].bold = True
+# #
+# #     # Add the data to the table
+# #     for i in range(df.shape[0]):
+# #         for j in range(df.shape[-1]):
+# #
+# #   
+# #
+# #             print(i,j)
+# #
+# #             #!~~~~~~~~~~~~~~~~~
+# #             # Add the cell data to the table
+# #             doc.tables[0].cell(i + 1, j).text = str(df.values[i, j])
+# #             print(doc.tables[0].cell(i + 1, j).text)
+# #
+# #
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import docx
 import adding_objects_file
 import send_mail_tab
@@ -8,29 +9,29 @@ import pandas as pd
 doc = docx.Document("input_word_file.docx")
 
 # Adding a picture to the Word document
-adding_objects_file.Add_pic(doc,pic_name="pic1.JPG",pic_description="pic is description of pic1")
+adding_objects_file.Add_pic(doc, pic_name="pic1.JPG", pic_description="pic is description of pic1")
 
 # Adding another picture to the Word document
-adding_objects_file.Add_pic(doc,pic_name="pic2.PNG",pic_description="pic is description of pic2")
+adding_objects_file.Add_pic(doc, pic_name="pic2.PNG", pic_description="pic is description of pic2")
 
-# Reading the data from the excel file into a pandas dataframe, skipping rows 0,1,3 in the input excel file
-table1=f"DATABASES/tourism_israel.xlsx"
-df = pd.read_excel(table1,sheet_name="2.2.28", skiprows=[0,1,2])
+# Reading the data from the excel file into a pandas dataframe, skipping rows 0,1,2 in the input excel file
+table1 = f"DATABASES/tourism_israel.xlsx"
+df = pd.read_excel(table1, sheet_name="2.2.28", skiprows=[0, 1, 2])
 
 # Reading the contact information from another excel file into a pandas dataframe
-table2=f"DATABASES/דף_קשר.xlsx"
-df_contacts = pd.read_excel(table2,sheet_name="אנשי קשר", skiprows=[0])
+table2 = f"DATABASES/דף_קשר.xlsx"
+df_contacts = pd.read_excel(table2, sheet_name="אנשי קשר", skiprows=[0])
 
 # Adding the dataframe as a table to the Word document
-adding_objects_file.Add_table(doc,df,subtitle_to_the_table="this is subtitle")
+adding_objects_file.Add_table(doc, df, subtitle_to_the_table="this is subtitle")
 
 # Saving the changes to the Word document
 try:
     # Try to save the file as "doc1.docx"
-    doc_to_attach=doc.save("doc1.docx")
+    doc_to_attach = doc.save("doc1.docx")
 except PermissionError:
     # If there is a permission error, save the file as "doc2.docx"
-    doc_to_attach=doc.save("doc2.docx")
+    doc_to_attach = doc.save("doc2.docx")
 
 # Convert the df_contacts dataframe to a list of dictionaries and send emails to each person
 dict_list = df_contacts.to_dict('records')
@@ -41,7 +42,4 @@ for num in range(0, len(dict_list)):
     company_name = dict_list[num]['חברה']
 
     # Call the send_mail function to send an email to each person in the list
-    send_mail_tab.send_mail(mail,name,company_name,"doc1.docx")
-
-
-
+    send_mail_tab.send_mail(mail, name, company_name, "doc1.docx")
