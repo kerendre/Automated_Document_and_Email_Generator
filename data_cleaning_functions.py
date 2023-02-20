954a29ef3d8b0b69663ef53089470707da404ddc
Drevin Keren <drevinkeren@gmail.com>
2023-02-20 13:11:36 +0200

    fully working project
    "This commit adds the following features to the project:
    
        *Creates an empty Word document named 'doc'.
        *Adds a header to the document with links to my GitHub and LinkedIn profiles.
        *Adds two pictures ('pic1' and 'pic2') from "PICS/..."
        *Creates a Pandas DataFrame from "DATABASES/...", cleans and merges data from various databases.
        *Drops all Hebrew columns, reorganizes the table based on the 'municipal' column value, and adds 'name' and 'email address' columns to the DataFrame using another DataFrame named 'df_contact'.
        *Adds the resulting DataFrame to doc
        *Save doc to a Word document named 'doc1.docx'."
    
    This revision clarifies the different features added by the commit and uses clearer language to describe the actions taken.

--- data_cleaning_functions.py
+++ data_cleaning_functions.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 def filter_df(df):
     # Get the list of column names that don't end in "heb" or "heb "
@@ -8,3 +9,26 @@ def filter_df(df):
 
     # Return the new DataFrame
     return df1
+
+
+def format_dates_2_d_m_Y(df, column_name):
+    # Convert the specified column to a datetime object if it's not already in that format
+    if not pd.api.types.is_datetime64_any_dtype(df[column_name]):
+        df[column_name] = pd.to_datetime(df[column_name])
+
+    # Convert the specified column to a string in the desired format
+    df[column_name] = df[column_name].dt.strftime("%d/%m/%Y")
+
+    # Return the modified dataframe
+    return df
+
+
+
+def merge_data(df, df_contact,on_column):
+    # Merge the two dataframes on the "municipal" column
+    merged_df = pd.merge(df, df_contact, on=on_column)
+
+    # Return the merged dataframe
+    return merged_df
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import data_cleaning_functions
 
 import docx
 import adding_objects_file
@@ -16,12 +16,12 @@ pic2_description="pic2_description"
 table1_origin_location_n_name="DATABASES/input_xlsx_file.xlsx" # tourism_israel.xlsx
 table1_sheet_name="Sheet1"
 table1_number_of_rows_2_skip=[]
-subtitle_to_the_table="This is subtitle to  the table "
+subtitle_to_the_table1="This is subtitle to  the table "
 
+table2_origin_location_n_name = "DATABASES/Contact_Info.xlsx"
+table2_sheet_name="Sheet1"
+table2_number_of_rows_2_skip=[]
 
-#table2_origin_location_n_name="DATABASES/דף_קשר.xlsx"
-#table2_sheet_name="אנשי קשר"
-table2_number_of_rows_2_skip=[0]
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Open the Word document
@@ -37,35 +37,45 @@ adding_objects_file.Add_pic(doc, pic_name=pic1, pic_description=pic1_description
 # Adding another picture to the Word document
 adding_objects_file.Add_pic(doc, pic_name=pic2, pic_description=pic2_description)
 
-# Reading the data from the excel file into a pandas dataframe, skipping rows 0,1,2 in the input excel file
+# Reading the data from the excel file into a pandas dataframe)
 df = pd.read_excel(table1_origin_location_n_name, sheet_name=table1_sheet_name, skiprows=table1_number_of_rows_2_skip)
 
 
-
+df = data_cleaning_functions.format_dates_2_d_m_Y(df, "first_visi")
+df = data_cleaning_functions.format_dates_2_d_m_Y(df, "second_vis")
+df = data_cleaning_functions.format_dates_2_d_m_Y(df, "third_visi")
 
 # Reading the contact information from another excel file into a pandas dataframe
-df_contacts = pd.read_excel("DATABASES/דף_קשר.xlsx", sheet_name="אנשי קשר", skiprows=table2_number_of_rows_2_skip)
+df_contacts = pd.read_excel(table2_origin_location_n_name, sheet_name=table2_sheet_name, skiprows=table2_number_of_rows_2_skip)
+
+# 19.02.23 finished here doesnt work the marge
+
+
+#on_column="municipal"
+#merged_df=data_cleaning_functions.merge_data(df, df_contacts,on_column)
+#print(merged_df)
 
 # Adding the dataframe as a table to the Word document
-doc =adding_objects_file.Add_table(doc, df, subtitle_to_the_table=subtitle_to_the_table)
+doc =adding_objects_file.Add_table(doc, df, subtitle_to_the_table=subtitle_to_the_table1)
 
 # add a hyperlink to the header
 adding_objects_file.add_hyperlink_to_header(doc, 'https://www.linkedin.com/in/keren-drevin/', 'Linkdein/Keren Drevin')
 adding_objects_file.add_hyperlink_to_header(doc, 'https://github.com/kerendre', '                                                                              GitHub/Keren Drevin')
 adding_objects_file.add_hyperlink_to_header(doc, 'https://www.linkedin.com/in/keren-drevin/', "\n ________________________________________________________________________________________________________")
 
-
 # Try to save the file as "doc1.docx"
 doc_to_attach = doc.save("doc1.docx")
 
 
 # Convert the df_contacts dataframe to a list of dictionaries and send emails to each person
-dict_list = df_contacts.to_dict('records')
-for num in range(0, len(dict_list)):
-    dict = (dict_list[num])
-    name = dict_list[num][' שם']
-    mail = dict_list[num]['מייל']
-    company_name = dict_list[num]['חברה']
+dict_list = df_contacts.to_dict(orient='records')
 
+for num in range(0, len(dict_list)):
+    #dict = (dict_list[num])
+    print((dict_list[num], num))
+
+    name = dict_list[num]['name']
+    mail = dict_list[num]['mail']
+    company_name = dict_list[num]['municipal']
     # Call the send_mail function to send an email to each person in the list
     #send_mail_tab.send_mail(mail, name, company_name, "doc1.docx")
